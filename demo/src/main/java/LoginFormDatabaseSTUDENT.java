1e3ee54d7c50b7320e8f20b65589fcc767180aee
Maria Griffin <maria.griffin@atu.ie>
2023-02-02 21:55:46 +0000

    Add files via upload
    Initial Code Commit

---
+++ demo/src/main/java/LoginFormDatabaseSTUDENT.java
@@ -0,0 +1,230 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class LoginFormDatabaseSTUDENT extends javax.swing.JFrame {
+
+	private javax.swing.JButton btnLogin;
+	private javax.swing.JLabel jLabel1;
+	private javax.swing.JLabel jLabelName;
+	private javax.swing.JLabel jLabelPswrd;
+	private javax.swing.JLabel jLabelDept;
+	private javax.swing.JPanel jPanel1;
+	private javax.swing.JPasswordField txtPwd;
+	private javax.swing.JTextField txtUsername;
+	private javax.swing.JTextField txtDept;
+	/**
+	 * Creates new form LoginForm
+	 */
+
+	Connection conn = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+
+	public LoginFormDatabaseSTUDENT() {
+		initComponents();
+		setTitle("Login Form");
+
+		btnLogin.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				// What changes can you add here to make this login more secure?
+//part 1		// Validate that the username and password match an eentry in the database?
+				// Output an appropriate message for incorrect details
+				// Take a look at Rule: MET00-J. Validate method arguments what other check do
+				// you need to add??
+
+				String usernameEntered = txtUsername.getText();
+				String passwordEntered = txtPwd.getText();				
+				String departmentEntered = txtDept.getText();
+
+
+				if (usernameEntered.isEmpty() || passwordEntered.isEmpty()) {
+					JOptionPane.showMessageDialog(LoginFormDatabaseSTUDENT.this, "Please enter a username and password User Details");
+				} else {
+
+					try {
+
+						conn = dbConnect();
+						System.out.println("Creating statement...");
+						
+//PART 2 A			//there is a more secure way to do this, using a prepared statement
+					//what coding guideline do we need to observe here?
+					//Fix this code to use a prepared Statement
+						String sql = "SELECT * FROM users where department = '" + departmentEntered + "'" ;    //Change this to use a prepared statement
+						System.out.println("sql" + sql);	
+						stmt = conn.createStatement();
+						rs = stmt.executeQuery(sql);
+
+						while (rs.next()) {				
+							System.out.println("Users available  " + rs.getString("username"));					
+						}
+						
+						rs = stmt.executeQuery(sql);             //!!NOT GOOD PRACTICE - THIS IS FOR DEMO PURPOSES ONLY!!
+						boolean matchFound = false;
+						while (rs.next()) {
+					//Validate that the username and password match an entry in the database
+//PART 2 B		//there are no errors in the code, yet this is not working why????
+				//What coding Guideline do we need to follow here??
+
+//PART 3 C		// Take a look at Rule: MET00-J. Validate method arguments 
+				//what other check is best practice to add here??
+							if (usernameEntered == rs.getString("username") && passwordEntered == rs.getString("password")) {
+								JOptionPane.showMessageDialog(LoginFormDatabaseSTUDENT.this, "Valid User: " + usernameEntered);
+								 matchFound = true;
+								break;
+							} 							
+						}
+						if (matchFound == false) {
+							JOptionPane.showMessageDialog(LoginFormDatabaseSTUDENT.this, "Invalid User Details");
+						}
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+
+				// STEP 6: Clean-up environment
+				dbClose();
+			}
+
+		});
+	}
+
+	public static void main(String args[]) {
+
+		/* Create and display the form */
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new LoginFormDatabaseSTUDENT().setVisible(true);
+
+			}
+		});
+	}
+
+	/**
+	 * This method is called to connect to the database. WARNING: Do NOT modify this
+	 * code.
+	 */
+	private Connection dbConnect() {
+
+		// database URL and credentials
+		final String DB_URL = "jdbc:mysql://localhost:3306/mysecureapplication";
+		final String USER = "newuser";
+		final String PASS = "Password@22";
+
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		}
+
+		return conn;
+
+	}
+
+	/**
+	 * This method is called to connect to the database. WARNING: Do NOT modify this
+	 * code.
+	 */
+	private void dbClose() {
+		try {
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * This method is called from within the constructor to initialize the form.
+	 * WARNING: Do NOT modify this code.
+	 */
+	private void initComponents() {
+
+		// jPanel1 = new javax.swing.JPanel();
+		jLabel1 = new javax.swing.JLabel();
+		jLabelName = new javax.swing.JLabel();
+		jLabelPswrd = new javax.swing.JLabel();
+		jLabelDept = new javax.swing.JLabel();
+		btnLogin = new javax.swing.JButton();
+		txtUsername = new javax.swing.JTextField();
+		txtPwd = new javax.swing.JPasswordField();
+		txtDept = new javax.swing.JTextField();
+
+		jLabelName.setText("Username");
+		jLabelPswrd.setText("Password");
+		jLabelDept.setText("Department");
+		btnLogin.setText("login");
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		jLabel1.setText("LYIT Login");
+		jPanel1 = new JPanel(new GridBagLayout());
+
+		GridBagConstraints cs = new GridBagConstraints();
+		cs.fill = GridBagConstraints.HORIZONTAL;
+
+		cs.gridx = 0;
+		cs.gridy = 0;
+		cs.gridwidth = 1;
+		jPanel1.add(jLabelName, cs);
+
+		cs.gridx = 1;
+		cs.gridy = 0;
+		cs.gridwidth = 25;
+		jPanel1.add(txtUsername, cs);
+
+		cs.gridx = 0;
+		cs.gridy = 1;
+		cs.gridwidth = 1;
+		jPanel1.add(jLabelPswrd, cs);
+
+		cs.gridx = 1;
+		cs.gridy = 1;
+		cs.gridwidth = 25;
+		jPanel1.add(txtPwd, cs);
+		
+		cs.gridx = 0;
+		cs.gridy = 2;
+		cs.gridwidth = 1;
+		jPanel1.add(jLabelDept, cs);
+
+		cs.gridx = 2;
+		cs.gridy = 2;
+		cs.gridwidth = 25;
+		jPanel1.add(txtDept, cs);
+
+		cs.gridx = 3;
+		cs.gridy = 3;
+		cs.gridwidth = 2;
+		jPanel1.add(btnLogin, cs);
+
+		getContentPane().add(jPanel1, BorderLayout.CENTER);
+		pack();
+		setLocationRelativeTo(null);
+		setSize(270, 250);
+
+	}
+}
