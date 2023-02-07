c2c2901c36f9c983a40325c70c4041b0275ad3fb
Maria Griffin <maria.griffin@atu.ie>
2023-02-07 11:35:43 +0000

    adding database file
    file to create a database table

---
+++ mysecureapplication.sql
@@ -0,0 +1,59 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mysecureapplication`
+--
+
+DROP DATABASE IF EXISTS `mysecureapplication`;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `mysecureapplication` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `mysecureapplication`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `department` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `department`) VALUES
+('adam.apple', 'password', 'business'),
+('dan.doughnut', 'password', 'business'),
+('john.jones', 'password', 'computing'),
+('mary.murphy', 'password', 'computing'),
+('ray.royal', 'password', 'business'),
+('sam.son', 'password', 'computing'),
+('tom.tank', 'password', 'computing');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `username` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
